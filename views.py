5986dbbba08228ba2fc57c206eb2c6e88d08f52a
Junior Ohanyere <junohanyere@gmail.com>
2024-07-08 11:01:10 +0100

    views module

---
+++ views.py
@@ -0,0 +1,24 @@
+"""Module to handle views.
+"""
+
+import asyncio
+import curses
+from curses import panel
+import os
+
+
+def signin(cache_file):
+    """Sign in view.
+    """
+
+    if not os.access(cache_file, F_OK):
+
+
+def signup():
+    """Sign up view.
+    """
+
+
+def auth():
+    """Authenticate view.
+    """
